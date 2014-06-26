72992f70431521911d441f7d97cca20c6d6cb474
Eric Kelly <heroiceric@gmail.com>
2014-06-26 10:53:23 -0400

    Add posts

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'pages#index'
+  root 'posts#index'
+
+  resources :posts, only: [:show]
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,25 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+post_attrs = [
+  {
+    title: 'Heartbeats',
+    body: %{I believe every human has a finite number of heartbeats. I don\'t
+            intend to waste any of mine}
+  }, {
+    title: 'Pilot Health',
+    body: %{You know, being a test pilot isn't always the healthiest business
+            in the world.}
+  }, {
+    title: 'Tiny Pea',
+    body: %{It suddenly struck me that that tiny pea, pretty and blue, was the
+            Earth. I put up my thumb and shut one eye, and my thumb blotted out
+            the planet Earth. I didn't feel like a giant. I felt very, very
+            small.}
+  }, {
+    title: 'Pilot Health',
+    body: %{To be the first to enter the cosmos, to engage, single-handed, in an
+            unprecedented duel with nature—could one dream of anything more?}
+  }
+]
+
+post_attrs.each do |attrs|
+  Post.find_or_create_by!(attrs)
+end
